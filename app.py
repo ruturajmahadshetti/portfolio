2e4f68224c7f20e5075536d0cb4f9a2e6f4760b4
ruturajmahadshetti <ruturajm9770@gmail.com>
2025-04-18 22:47:03 +0900

    fix: bind flask app to 0.0.0.0

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask,render_template
 import pandas as pd
-
+import os
 app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
 @app.route('/publications')
 def publications():
-    df = pd.read_csv('./publications.csv')
-    journal_pubs = df['journal'].dropna().tolist()
-    conf_pubs = df['conference'].dropna().tolist()
+    j_df = pd.read_csv('./docs/journal.csv')
+    c_df = pd.read_csv('./docs/conference.csv')
+    journal_pubs = j_df.to_dict(orient='records')
+    conf_pubs = c_df['paper'].dropna().to_list()
     return render_template('pub.html', journals=journal_pubs, conferences=conf_pubs)
     return render_template('pub.html')
 
@@ -20,5 +21,5 @@ def projects():
 def contact():
     return render_template("contact_info.html")
 if __name__=="__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
